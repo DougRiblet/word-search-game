717fed3385376f1eeb63b53f883dc2bae91f872c
Doug Riblet <smashzen85@gmail.com>
2018-02-14 09:40:50 -0600

    build Input-Four component

--- client/components/Input-Four/input-four-present.jsx
+++ client/components/Input-Four/input-four-present.jsx
@@ -6,16 +6,27 @@ export default class InputFour extends React.Component {
     this.state = {
       'guess': ''
     }
-    this.handleGuess = this.handleGuess.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange (event) {
-    this.setState({'guess': event.target.value})
+    this.setState({value: event.target.value.toUpperCase()});
+  }
+
+  handleSubmit (event) {
+    if ()
+    event.preventDefault();
   }
 
   render () {
     return (
       <div id='enter-guess'>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+            value={this.state.guess}
+            onChange={this.handleChange} />
+        </form>
       </div>
     )
   }
