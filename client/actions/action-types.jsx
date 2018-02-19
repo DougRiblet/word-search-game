b96b2797edf3190e319630bda41a016b2226b29f
Doug Riblet <smashzen85@gmail.com>
2018-02-19 15:56:29 -0600

    add flow, type-checking action-types

--- client/actions/action-types.jsx
+++ client/actions/action-types.jsx
@@ -1,5 +1,7 @@
-export const GUESS_RIGHT = 'GUESS_RIGHT';
-export const GUESS_WRONG = 'GUESS_WRONG';
-export const REQUEST_NEW_GAME = 'REQUEST_NEW_GAME';
-export const RECEIVE_NEW_GAME = 'RECEIVE_NEW_GAME';
-export const END_GAME = 'END_GAME';
+// @flow
+
+export const GUESS_RIGHT: string = 'GUESS_RIGHT';
+export const GUESS_WRONG: string = 'GUESS_WRONG';
+export const REQUEST_NEW_GAME: string = 'REQUEST_NEW_GAME';
+export const RECEIVE_NEW_GAME: string = 'RECEIVE_NEW_GAME';
+export const END_GAME: string = 'END_GAME';
