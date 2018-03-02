ac0b63055c6da2090f6cde1caf9529cc5fe96a3e
Doug Riblet <smashzen85@gmail.com>
2018-03-02 00:10:02 -0600

    write enzyme test for quiz-seven component

--- client/__tests__/quiz-seven-tests.js
+++ client/__tests__/quiz-seven-tests.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import CurrentSeven from '../components/Quiz-Seven/quiz-seven-present';
 
 describe('Quiz Seven', () => {
@@ -15,5 +16,11 @@ describe('Quiz Seven', () => {
     const div = document.createElement('div');
     ReactDOM.render(<CurrentSeven currentseven='JEEZELY' />, div);
   });
+
+  test('displays current seven-letter word', () => {
+    const wrapper = shallow(<CurrentSeven currentseven='JEEZELY' />);
+    expect(wrapper.text()).toEqual('JEEZELY');
+    expect(wrapper.html()).toEqual('<div id="quiz-seven"><h1>JEEZELY</h1></div>');
+  })
 
 });
