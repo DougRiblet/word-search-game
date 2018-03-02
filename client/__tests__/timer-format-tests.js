944ddcf52cf968177dede16c799985a4a42b26d9
Doug Riblet <smashzen85@gmail.com>
2018-03-01 19:02:44 -0600

    write test for timer-format component

--- client/__tests__/timer-format-tests.js
+++ client/__tests__/timer-format-tests.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import TimerFormat from '../components/Timer-Clock/timer-format';
 
 describe('Timer Clock', () => {
@@ -14,6 +15,12 @@ describe('Timer Clock', () => {
   test('renders without crashing with timer off', () => {
     const div = document.createElement('div');
     ReactDOM.render(<TimerFormat secondsleft={140} />, div);
+  });
+
+  test('converts number of seconds into correct string', () => {
+    const wrapper = shallow(<TimerFormat secondsleft={140} />);
+    expect(wrapper.text()).toEqual('2:20');
+    expect(wrapper.html()).toEqual('<span>2:20</span>');
   });
 
 });
