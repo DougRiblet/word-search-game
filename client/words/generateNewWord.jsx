75d052192ce815ebb6de7f27704f8e163a6d1841
Doug Riblet <smashzen85@gmail.com>
2018-03-10 01:41:09 -0600

    fix linting errors in words folder

--- client/words/generateNewWord.jsx
+++ client/words/generateNewWord.jsx
@@ -5,9 +5,9 @@ const testFour = (alphaSeven, four) => {
   const alphaFour = four.split('').sort();
   let indexInSeven = 0;
   let match = 0;
-  for (let i=0; i<4; i+=1){
-    for (let j=indexInSeven; j<7; j+=1){
-      if (alphaFour[i] === alphaSeven[j]){
+  for (let i = 0; i < 4; i += 1) {
+    for (let j = indexInSeven; j < 7; j += 1) {
+      if (alphaFour[i] === alphaSeven[j]) {
         match += 1;
         indexInSeven = j + 1;
         break;
@@ -18,30 +18,33 @@ const testFour = (alphaSeven, four) => {
 };
 
 const generateNewWord = () => {
-  let newSeven = sevens[Math.floor(Math.random() * sevens.length)];
-  let alphaSeven = newSeven.split('').sort();
-  let newPool = [];
+  const newSeven = sevens[Math.floor(Math.random() * sevens.length)];
+  const alphaSeven = newSeven.split('').sort();
+  const newPool = [];
 
-  fours.forEach(function(item){
+  fours.forEach((item) => {
     if (testFour(alphaSeven, item)) {
       newPool.push(item);
     }
   });
 
-  let newLength = newPool.length;
-  let newSecs = 10 + (newLength * 5);
-  let output = {newSeven, newPool, newLength, newSecs};
+  const newLength = newPool.length;
+  const newSecs = 10 + (newLength * 5);
+  const output = {
+    newSeven,
+    newPool,
+    newLength,
+    newSecs,
+  };
 
-  return new Promise(
-    (resolve, reject) => {
-      if (output) {
-        resolve(output);
-      } else {
-        const reason = new Error('Error starting new game');
-        reject(reason);
-      }
+  return new Promise((resolve, reject) => {
+    if (output) {
+      resolve(output);
+    } else {
+      const reason = new Error('Error starting new game');
+      reject(reason);
     }
-  );
+  });
 };
 
 export default generateNewWord;
